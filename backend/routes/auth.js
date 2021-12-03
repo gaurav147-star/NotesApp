e5a8cf13c5b54f2f2f7d0a1ab802ed414f8f3534
gaurav147-star <gauravgupta9158@gmail.com>
2021-12-03 21:42:43 +0530

    f-hash and jwt is used

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "Gauravisagoodb$oy";
 
 // Create a User using: POST "/api/auth/createuser". Doesn't require Auth
 router.post(
@@ -27,15 +31,27 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      //Create a new user
       user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPass,
         email: req.body.email,
       });
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      // res.json(user);
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some Error occured")
+      res.status(500).send("Some Error occured");
     }
   }
 );
